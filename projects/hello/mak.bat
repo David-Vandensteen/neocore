659870407c7c69025aafd05a6fbf0c39d2d7865e
David Vandensteen <david.vandensteen@leroymerlin.fr>
2019-06-27 13:20:36 +0200

    kill fix, add hot-reload script

--- projects/hello/mak.bat
+++ projects/hello/mak.bat
@@ -1,7 +1,7 @@
 @echo off
 set backupPath=%path%
 set NEODEV=%appdata%\neocore\neodev-sdk
-path=%NEODEV%\m68k\bin;%appdata%\neocore\bin;%windir%\System32\WindowsPowerShell\v1.0\
+path=%NEODEV%\m68k\bin;%appdata%\neocore\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
 @echo on
 make %1 %2 %3
 @echo off
