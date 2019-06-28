cb694b3fd94140cd94255f118f6535dc55f0fdb9
Nainain <dvandensteen@gmail.com>
2019-06-28 19:05:10 +0200

    refactoring

--- projects/hello/mak.bat
+++ projects/hello/mak.bat
@@ -1,8 +1,9 @@
 @echo off
+set PROJECT=hello
 set backupPath=%path%
 set NEODEV=%appdata%\neocore\neodev-sdk
 path=%NEODEV%\m68k\bin;%appdata%\neocore\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
 @echo on
-make %1 %2 %3
+make -f ..\Makefile %1 %2 %3
 @echo off
 path=%backupPath%
--- scripts/neocore-init-project.ps1
+++ scripts/neocore-init-project.ps1
@@ -17,7 +17,7 @@ function copyFile([boolean] $_exit, [String] $_src, [String] $_dest){
   }
 }
 
-function copyCrt{
+function copyFiles{
   copyFile 0 scaffolding\common_crt0_cd.s projects\$projectName
   copyFile 0 scaffolding\crt0_cd.s projects\$projectName
   copyFile 0 scaffolding\mak.bat projects\$projectName
@@ -30,22 +30,29 @@ function configureMakefile{
   [System.IO.File]::WriteAllText("projects\$projectName\Makefile", $content)
 }
 
+function configureMak{
+  Write-Host "Configure mak"
+  $content = [System.IO.File]::ReadAllText("projects\$projectName\mak.bat").Replace("/*project*/","$projectName")
+  [System.IO.File]::WriteAllText("projects\$projectName\mak.bat", $content)
+}
+
 function configureMain{
   Write-Host "Generate main.c for $projectName"
   copyFile 0 scaffolding\main.c projects\$projectName
 }
 
 function makeInit{
-  Write-Host "$projectName make init"
+  Write-Host "$projectName mak init"
   pushd projects\$projectName
-  & make init
+  & .\mak.bat init
   popd
 }
 
 function _main{
   createFolder projects\$projectName
-  copyCrt
-  configureMakefile
+  copyFiles
+  #configureMakefile
+  configureMak
   configureMain
   makeInit
 }
