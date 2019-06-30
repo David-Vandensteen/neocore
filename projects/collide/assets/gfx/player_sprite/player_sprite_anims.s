edc4fb0a818ed905840f95f8c1731db0ac442551
Nainain <dvandensteen@gmail.com>
2019-06-30 20:07:49 +0200

    collide

---
+++ projects/collide/assets/gfx/player_sprite/player_sprite_anims.s
@@ -0,0 +1,27 @@
+player_sprite_animations:
+player_sprite_anim_IDLE:
+	.word	0x0004, 0x0000	|; 4 steps, 0 repeats
+	.long	player_sprite_anim_IDLE_steps, player_sprite_anim_IDLE	|; steplist, link
+player_sprite_anim_UP:
+	.word	0x0001, 0x0000	|; 1 steps, 0 repeats
+	.long	player_sprite_anim_UP_steps, 0x00000000	|; steplist, link
+player_sprite_anim_DOWN:
+	.word	0x0001, 0x0000	|; 1 steps, 0 repeats
+	.long	player_sprite_anim_DOWN_steps, 0x00000000	|; steplist, link
+
+player_sprite_anim_IDLE_steps:
+	.long	player_sprite_0000	|;frame ptr
+	.word	0x0000, 0x0000, 0x0005	|;shiftX, shiftY, duration
+	.long	player_sprite_0001	|;frame ptr
+	.word	0x0000, 0x0000, 0x0005	|;shiftX, shiftY, duration
+	.long	player_sprite_0002	|;frame ptr
+	.word	0x0000, 0x0000, 0x0005	|;shiftX, shiftY, duration
+	.long	player_sprite_0001	|;frame ptr
+	.word	0x0000, 0x0000, 0x0005	|;shiftX, shiftY, duration
+player_sprite_anim_UP_steps:
+	.long	player_sprite_0003	|;frame ptr
+	.word	0x0000, 0x0000, 0x0005	|;shiftX, shiftY, duration
+player_sprite_anim_DOWN_steps:
+	.long	player_sprite_0004	|;frame ptr
+	.word	0x0000, 0x0000, 0x0005	|;shiftX, shiftY, duration
+
---
+++ projects/collide/main.c
@@ -0,0 +1,46 @@
+#include <neocore.h>
+#include "externs.h"
+
+typedef struct bkp_ram_info {
+	WORD debug_dips;
+	BYTE stuff[254];
+	//256 bytes
+} bkp_ram_info;
+
+bkp_ram_info bkp_data;
+
+int main(void) {
+  aSpritePhysic player;
+  picturePhysic asteroid;
+  gpuInit();
+  player = aSpritePhysicDisplayAutobox(&player_sprite, &player_sprite_Palettes, 10, 10, 8, 0, PLAYER_SPRITE_ANIM_IDLE);
+  asteroid = picturePhysicDisplayAutobox(&asteroid_sprite, &asteroid_sprite_Palettes, 100, 100);
+  while(1) {
+    waitVBlank();
+    joypadUpdate();
+
+    if (joypadIsLeft() && player.as.posX > 0) { aSpritePhysicMove(&player, -1, 0); }
+    if (joypadIsRight() && player.as.posX < 280) { aSpritePhysicMove(&player, 1, 0); }
+    if (joypadIsUp() && player.as.posY > 0) {
+      aSpritePhysicMove(&player, 0, -1);
+      aSpriteSetAnim(&player.as, PLAYER_SPRITE_ANIM_UP);
+    }
+    if (joypadIsDown() && player.as.posY < 200) {
+      aSpritePhysicMove(&player, 0, 1);
+      aSpriteSetAnim(&player.as, PLAYER_SPRITE_ANIM_DOWN);
+    }
+    if (!joypadIsDown() && !joypadIsUp()) { aSpriteSetAnim(&player.as, PLAYER_SPRITE_ANIM_IDLE); }
+
+    if (boxCollide(&player.box, &asteroid.box)) {
+      aSpritePhysicFlash(&player, true, 5);
+    } else {
+      aSpritePhysicFlash(&player, false, 0);
+    }
+    aSpritePhysicFlashUpdate(&player);
+
+    aSpriteAnimate(&player);
+    SCClose();
+  };
+	SCClose();
+  return 0;
+}
