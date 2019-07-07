dbc4d4fd097065daa68f4163fbb2a0e0c453268f
David Vandensteen <david.vandensteen@leroymerlin.fr>
2019-07-07 13:52:05 +0200

    return errorlevel

--- projects/shrunk/mak.bat
+++ projects/shrunk/mak.bat
@@ -3,13 +3,13 @@ set PROJECT=shrunk
 set backupPath=%path%
 set NEODEV=%appdata%\neocore\neodev-sdk
 path=%NEODEV%\m68k\bin;%appdata%\neocore\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
+set error=0
 @echo on
 make -f ..\Makefile %1 %2 %3
-@echo off
 if %errorlevel% neq 0 (
-    @echo off
-    path=%backupPath%
-    exit /b %errorlevel%
+  @echo off
+  set error=%errorlevel%
 )
 @echo off
 path=%backupPath%
+exit /b %error%
--- scaffolding/mak.bat
+++ scaffolding/mak.bat
@@ -5,11 +5,9 @@ set NEODEV=%appdata%\neocore\neodev-sdk
 path=%NEODEV%\m68k\bin;%appdata%\neocore\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
 @echo on
 make -f ..\Makefile %1 %2 %3
-@echo off
 if %errorlevel% neq 0 (
-    @echo off
-    path=%backupPath%
-    exit /b %errorlevel%
+  @echo off
+  set error=%errorlevel%
 )
 @echo off
 path=%backupPath%
