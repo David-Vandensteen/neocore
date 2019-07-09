d9a7300ec5d3e9f9dea0e13b50bbc969b5deea19
David Vandensteen <david.vandensteen@leroymerlin.fr>
2019-07-09 13:50:24 +0200

    wip

--- projects/shrunk-centroid/main.c
+++ projects/shrunk-centroid/main.c
@@ -22,16 +22,20 @@ static void update();
 static void update_laser1();
 static void update_laser2();
 
+static void pictureShrunkCentroid(picture *p, pictureInfo *pi, WORD shrunk_value);
+
 static void display() {
   paletteDisableAutoinc();
-  laser1 = pictureDisplay(&laser_sprite, &laser_sprite_Palettes, fixtoi(laser1_position[X]), fixtoi(laser1_position[Y]));
+  // laser1 = pictureDisplay(&laser_sprite, &laser_sprite_Palettes, fixtoi(laser1_position[X]), fixtoi(laser1_position[Y]));
   laser2 = pictureDisplay(&laser_sprite, &laser_sprite_Palettes, fixtoi(laser2_position[X]), fixtoi(laser2_position[Y]));
   paletteEnableAutoinc();
 }
 
 static void update() {
-  if (DAT_frameCounter % 5 == 0) update_laser1();
-  if (DAT_frameCounter % 30 == 0) update_laser2();
+  // if (DAT_frameCounter % 5 == 0) update_laser1();
+  // if (DAT_frameCounter % 30 == 0) update_laser2();
+  update_laser2();
+  loggerInit();
 }
 
 static void update_laser1() {
@@ -39,7 +43,6 @@ static void update_laser1() {
   laser1_position[X] += FIX((320 DIV32));
   pictureShrunk(&laser1, &laser_sprite, shrunkForge(laser1_shrunk_x, 0xFF));
   pictureSetPos(&laser1, fixtoi(laser1_position[X]), fixtoi(laser1_position[Y]));
-  loggerInit();
   loggerInt("X : ", fixtoi(laser1_position[X]));
   if (laser1_shrunk_x == 0) {
     laser1_shrunk_x = 0xF;
@@ -48,9 +51,17 @@ static void update_laser1() {
 }
 
 static void update_laser2() {
-  pictureShrunk(&laser2, &laser_sprite, shrunkPropTableGet(DAT_frameCounter));
-  // TODO maybe make a precalculed table with offset trim for centroid
+  pictureShrunkCentroid(&laser2, &laser_sprite, shrunkPropTableGet(DAT_frameCounter));
   // TODO add to the git tools for make tables
+}
+
+static void pictureShrunkCentroid(picture *p, pictureInfo *pi, WORD shrunk_value) {
+  // loggerByte("SHRUNK X : ", shrunk_value >> 8);
+  // loggerByte("SHRUNK Y : ", (BYTE)shrunk_value);
+  FIXED pos[2] = { FIX(p->posX), FIX(p->posY) };
+  pos[X] += FIX((320 DIV32));
+  pictureShrunk(p, pi, shrunk_value);
+  pictureSetPos(p, fixtoi(pos[X]), fixtoi(pos[Y]));
 }
 
 int main(void) {
