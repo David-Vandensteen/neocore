276aacb3078e4f1f04f2210a8221a545efc79573
David Vandensteen <david.vandensteen@leroymerlin.fr>
2019-07-09 13:51:38 +0200

    fix makefile path

--- mak.bat
+++ mak.bat
@@ -4,7 +4,7 @@ set NEODEV=%appdata%\neocore\neodev-sdk
 path=%NEODEV%\m68k\bin;%appdata%\neocore\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
 set error=0
 @echo on
-make -f ..\Makefile %1 %2 %3
+make -f Makefile %1 %2 %3
 if %errorlevel% neq 0 (
   @echo off
   set error=%errorlevel%
