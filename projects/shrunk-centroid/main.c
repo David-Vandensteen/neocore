647f6b48753241cc38bcddc3c1c3aa7a51541414
Nainain <dvandensteen@gmail.com>
2019-07-17 21:23:32 +0200

    refactoring, implement shrunk centroid

--- projects/shrunk-centroid/main.c
+++ projects/shrunk-centroid/main.c
@@ -10,97 +10,89 @@ typedef struct bkp_ram_info {
 
 bkp_ram_info bkp_data;
 
-#define SHRUNK_EXTRACT_X(value) value >> 8
-#define SHRUNK_EXTRACT_Y(value) (BYTE)value
+#define LOGO1_POSITION_CENTER_X_INIT 160
+#define LOGO3_POSITION_CENTER_Y_INIT 100
 
-static picture laser1, laser2, dot;
+static picture logo1, logo2, logo3;
 
-static BYTE laser1_shrunk_x = 0xF;
+static BYTE logo1_shrunk_x = 0xF;
+static short logo1_shrunk_inc = -1;
 
-static FIXED laser1_position_center[2] = { 160, 40 };
-static FIXED laser2_position_center[2] = { 160, 150 };
+static BYTE logo3_shrunk_y = 0xFF;
+static short logo3_shrunk_inc = -1;
+
+static vec2short logo1_position_center, logo2_position_center, logo3_position_center;
 
 static WORD shrunkTableIndex = 0;
 
+static void init();
 static void display();
 static void update();
-static void update_laser1();
-static void update_laser2();
+static void update_logo1();
+static void update_logo2();
+static void update_logo3();
 
-static void pictureShrunkCentroid(picture *p, pictureInfo *pi, short centerPosX, short centerPosY, WORD shrunk_value);
+static void init() {
+  logo1_position_center = vec2shortMake(LOGO1_POSITION_CENTER_X_INIT, 30);
+  logo2_position_center = vec2shortMake(160, 180);
+  logo3_position_center = vec2shortMake(160, LOGO3_POSITION_CENTER_Y_INIT);
+}
 
 static void display() {
   paletteDisableAutoinc();
-  laser1 = pictureDisplay(&laser_sprite, &laser_sprite_Palettes, fixtoi(laser1_position_center[X]), fixtoi(laser1_position_center[Y]));
+  logo1 = pictureDisplay(&logo_sprite, &logo_sprite_Palettes, logo1_position_center.x, logo1_position_center.y);
+  logo3 = pictureDisplay(&logo_sprite, &logo_sprite_Palettes, logo3_position_center.x, logo3_position_center.y);
   paletteEnableAutoinc();
-  laser2 = pictureDisplay(&laser_sprite, &laser_sprite_Palettes, laser2_position_center[X], laser2_position_center[Y]);
-  pictureDisplay(&dot_sprite, &dot_sprite_Palettes, laser2_position_center[X], laser2_position_center[Y]);
+  logo2 = pictureDisplay(&logo_sprite, &logo_sprite_Palettes, logo2_position_center.x, logo2_position_center.y);
+  loggerInit();
+  loggerInfo("HORIZONTAL SHRUNK");
+  loggerPositionSet(1, 11);
+  loggerInfo("VERTICAL SHRUNK");
+  loggerPositionSet(1, 20);
+  loggerInfo("PROPORTIONAL SHRUNK");
 }
 
 static void update() {
-  if (DAT_frameCounter % 5 == 0) update_laser1();
-//  if (DAT_frameCounter % 5 == 0) update_laser2();
-  update_laser2();
-
-  /*
-  loggerInit();
-  loggerShort("X : ", laser2.posX);
-  loggerShort("Y : ", laser2.posY);
-  loggerInfo("");
-  loggerShort("CX : ", laser2_position_center[X]);
-  loggerShort("CY : ", laser2_position_center[Y]);
-  loggerInfo("");
-  loggerWord("SH : ", shrunkPropTableGet(shrunkTableIndex));
-  loggerInfo("");
-  loggerByte("SH X : ", SHRUNK_EXTRACT_X(shrunkPropTableGet(shrunkTableIndex)));
-  loggerByte("SH Y : ", SHRUNK_EXTRACT_Y(shrunkPropTableGet(shrunkTableIndex)));
-  */
-
-  joypadUpdateEdge();
-  // if(joypadIsStart()) update_laser2();
+  if (DAT_frameCounter % 5 == 0) update_logo1();
+  update_logo2();
+  update_logo3();
 }
 
-static void update_laser1() {
-  laser1_shrunk_x--;
-  pictureShrunkCentroid(&laser1, &laser_sprite, laser1_position_center[X], laser1_position_center[Y], shrunkForge(laser1_shrunk_x, 0xFF));
-  if (laser1_shrunk_x == 0) {
-    laser1_shrunk_x = 0xF;
-    laser1_position_center[X] = 160;
+static void update_logo1() { // Horizontal
+  logo1_shrunk_x += logo1_shrunk_inc;
+  pictureShrunkCentroid(&logo1, &logo_sprite, logo1_position_center.x, logo1_position_center.y, shrunkForge(logo1_shrunk_x, 0xFF));
+  if (logo1_shrunk_x == 0) {
+    logo1_shrunk_inc = 1;
+    logo1_position_center.x = LOGO1_POSITION_CENTER_X_INIT;
+  } else {
+    if (logo1_shrunk_x >= 0xF) {
+      logo1_shrunk_inc = -1;
+    }
   }
 }
 
-static void update_laser2() {
-  pictureShrunkCentroid(&laser2, &laser_sprite, laser2_position_center[X], laser2_position_center[Y], shrunkPropTableGet(shrunkTableIndex));
-
+static void update_logo2() { // Proportional
+  pictureShrunkCentroid(&logo2, &logo_sprite, logo2_position_center.x, logo2_position_center.y, shrunkPropTableGet(shrunkTableIndex));
   shrunkTableIndex++;
   if (shrunkTableIndex >= SHRUNK_TABLE_PROP_SIZE) shrunkTableIndex = 0;
-  // TODO add to the git tools for make tables
 }
 
-static short shrunkCentroidGetTranslatedX(short centerPosX, WORD tileWidth, BYTE shrunkX) {
-  FIXED newX = FIX(centerPosX);
-  newX -= (shrunkX + 1) * FIX((tileWidth MULT8) / 0x10);
-  return fixtoi(newX);
-}
-
-static short shrunkCentroidGetTranslatedY(short centerPosY, WORD tileHeight, BYTE shrunkY) {
-  FIXED newY = FIX(centerPosY);
-  newY -= shrunkY * FIX((tileHeight MULT8) / 0xFF);
-  return fixtoi(newY);
-}
-
-static void pictureShrunkCentroid(picture *p, pictureInfo *pi, short centerPosX, short centerPosY, WORD shrunk_value) {
-  short position[2];
-  pictureShrunk(p, pi, shrunk_value);
-
-  position[X] = shrunkCentroidGetTranslatedX(centerPosX, pi->tileWidth, SHRUNK_EXTRACT_X(shrunk_value));
-  position[Y] = shrunkCentroidGetTranslatedY(centerPosY, pi->tileHeight, SHRUNK_EXTRACT_Y(shrunk_value));
-
-  pictureSetPos(p, position[X], position[Y]);
+static void update_logo3() { // Verical
+  logo3_shrunk_y += logo3_shrunk_inc;
+  pictureShrunkCentroid(&logo3, &logo_sprite, logo3_position_center.x, logo3_position_center.y, shrunkForge(0XF ,logo3_shrunk_y));
+  if (logo3_shrunk_y == 0) {
+    logo3_shrunk_inc = 1;
+    logo3_position_center.y = LOGO3_POSITION_CENTER_Y_INIT;
+  } else {
+    if (logo3_shrunk_y >= 0xFF) {
+      logo3_shrunk_inc = -1;
+    }
+  }
 }
 
 int main(void) {
   gpuInit();
+  init();
   display();
   while(1) {
     waitVBlank();
