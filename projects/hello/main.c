44d4c252c51bcbb3d3cc13086118da941fa58c2b
Nainain <dvandensteen@gmail.com>
2019-08-02 10:58:14 +0200

    upgrade neocore

--- projects/hello/main.c
+++ projects/hello/main.c
@@ -1,18 +1,12 @@
 /*
-	David Vandensteen
-	2019
+  David Vandensteen
+  2019
   <dvandensteen@gmail.com>
 */
 
 #include <neocore.h>
 
-typedef struct bkp_ram_info {
-	WORD debug_dips;
-	BYTE stuff[254];
-	//256 bytes
-} bkp_ram_info;
-
-bkp_ram_info bkp_data;
+NEOCORE_INIT
 
 int main(void) {
   gpuInit();
