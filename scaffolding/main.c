149712e2b40338005d101a21dc26409d628997f6
David Vandensteen <david.vandensteen@leroymerlin.fr>
2020-09-09 18:34:57 +0200

    Change : Improve workflow

--- scaffolding/main.c
+++ scaffolding/main.c
@@ -3,12 +3,28 @@
 
 NEOCORE_INIT
 
-int main(void) {
+static void init();
+static void display();
+static void update();
+
+static void init() {
   GPU_INIT
+}
+
+static void display() {
+
+}
+
+static void update() {
+  logger_init();
+  logger_info("HELLO NEO GEO !!!");
+}
+
+int main(void) {
+  init();
   while(1) {
     WAIT_VBL
-    logger_init();
-    logger_info("HELLO NEO GEO !!!");
+    update();
     SCClose();
   };
   SCClose();
