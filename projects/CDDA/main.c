6fbb3bb12ccd6cddde9c2e9b65b33ab066ee7ba0
David Vandensteen <david.vandensteen@leroymerlin.fr>
2020-09-23 17:47:06 +0200

    Add : Control the track number (left & right)

--- projects/CDDA/main.c
+++ projects/CDDA/main.c
@@ -12,9 +12,10 @@ static BOOL k7_direction = false;
 
 static Image k7, play, next, stop;
 static Scroller spectrum02;
+static BYTE track_num = 2;
 
 static void init() {
-  cdda_play(2);
+  cdda_play(track_num);
   GPU_INIT
   scroller_init(&spectrum02, &spectrum02_sprite, &spectrum02_sprite_Palettes);
   image_init(&k7, &k7_sprite, &k7_sprite_Palettes);
@@ -32,6 +33,9 @@ static void display() {
 }
 
 static void update() {
+  logger_init();
+  joypad_update_edge();
+  logger_byte("TRACK NUMBER : ", track_num);
   if (DAT_frameCounter % 2 == 0) {
     scroller_move(&spectrum02, 1, 0);
     if (spectrum02.s.scrlPosX > 960) scroller_set_position(&spectrum02, 0, spectrum02.s.scrlPosY);
@@ -45,8 +49,8 @@ static void update() {
   }
   if (k7.pic.posX > 50) k7_direction = false;
   if (k7.pic.posX < 40) k7_direction = true;
-  // image_move(&k7, -1, 0);
-  // image_set_position(&k7, (get_sin((sin_index DIV8) & 127)), k7.pic.posY);
+  if (joypad_is_left() && track_num > 2) cdda_play(--track_num);
+  if (joypad_is_right() && track_num < 4) cdda_play(++track_num);
 }
 
 int main(void) {
