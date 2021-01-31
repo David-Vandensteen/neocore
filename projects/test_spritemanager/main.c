aa95095fd28220f90d959d5ed113e012bbf06d1c
David Vandensteen <david.vandensteen@leroymerlin.fr>
2021-01-31 18:51:01 +0100

    Sync : test_spritemanager

--- projects/test_spritemanager/main.c
+++ projects/test_spritemanager/main.c
@@ -2,7 +2,6 @@
 #include <math.h>
 #include "externs.h"
 
-#define SPRITE_MANAGER_INDEX_MAX 20
 
 NEOCORE_INIT
 
@@ -10,63 +9,43 @@ static void init();
 static void display();
 static void update();
 
-static BOOL sprite_manager_index_status[SPRITE_MANAGER_INDEX_MAX];
-
-static void sprite_manager_init() {
-  WORD i = 0;
-  for (i = 0; i <= 10; i++) sprite_manager_index_status[i] = true;
-  for (i = 11; i < SPRITE_MANAGER_INDEX_MAX; i++) sprite_manager_index_status[i] = false;
-}
-
-static WORD sprite_manager_get_free(WORD max) {
-  // TODO : optimize
-  WORD i, j = 0;
-  BOOL found = false;
-  for( i = 0; i < SPRITE_MANAGER_INDEX_MAX; i++) {
-    if (!sprite_manager_index_status[i]) {
-      for (j = i; j < i + max; j++) {
-        if (!found) found = (!sprite_manager_index_status[j]) ? true : false;
-      }
-      if (found) return i;
-    }
-  }
-  return 0;
-}
-
-static void sprite_manager_set(WORD index) {
-  sprite_manager_index_status[index] = true;
-}
-
-static void sprite_manager_destroy(WORD index) {
-  sprite_manager_index_status[index] = false;
-}
-
-static void sprite_manager_debug() {
-  WORD i = 0;
-  logger_init();
-  for (i = 0; i < SPRITE_MANAGER_INDEX_MAX; i++) {
-    logger_bool(" ", sprite_manager_index_status[i]);
-  }
-  logger_word("FOUND : ", sprite_manager_get_free(3));
-}
+static Image planet01, planet02, planet03, planet04;
 
 static void init() {
   GPU_INIT
-  sprite_manager_init();
+  image_init(&planet01, &planet04_sprite, &planet04_sprite_Palettes);
+  image_init(&planet02, &planet04_sprite, &planet04_sprite_Palettes);
+  image_init(&planet03, &planet04_sprite, &planet04_sprite_Palettes);
+  image_init(&planet04, &planet04_sprite, &planet04_sprite_Palettes);
 }
 
 static void display() {
-
+  image_display(&planet01, 10, 10);
+  image_display(&planet02, 100, 100);
+  image_display(&planet03, 200, 100);
 }
 
 static void update() {
-  sprite_manager_debug();
+  logger_init();
+  logger_word("P1 INDEX : ", planet01.pic.baseSprite);
+  logger_info("MUST BE : 1");
+  logger_word("P2 INDEX : ", planet02.pic.baseSprite);
+  logger_info("MUST BE : 9");
+  logger_word("P3 INDEX : ", planet03.pic.baseSprite);
+  logger_info("MUST BE : 17");
+  if (DAT_frameCounter == 500) {
+    image_destroy(&planet01);
+    image_display(&planet04, 100, 10);
+  }
+  if (DAT_frameCounter > 500) {
+    logger_word("P4 INDEX : ", planet04.pic.baseSprite);
+    logger_info("MUST BE : 1");
+  }
 }
 
 int main(void) {
   init();
   display();
-  sprite_manager_set(12);
   while(1) {
     WAIT_VBL
     update();
