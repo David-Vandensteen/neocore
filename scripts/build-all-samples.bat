7aeb5dcc98476c1f4aeaa4afe230df926bc7b2f9
David Vandensteen <david.vandensteen@leroymerlin.fr>
2021-02-01 11:44:58 +0100

    Change : refactoring

--- scripts/build-all-samples.bat
+++
@@ -1,1 +0,0 @@
-powershell -ExecutionPolicy Bypass -File compile-run-tester.ps1 compile
--- scripts/compile-run-tester.ps1
+++ scripts/compile-run-tester.ps1
@@ -22,50 +22,58 @@ function runProject($name) {
   popd
 }
 
-function compileProjects {
-  compileProject "bullet"
-  compileProject "collide"
-  compileProject "collide_complex"
-  compileProject "collide_multiple"
-  compileProject "fixed_value"
-  compileProject "hello"
-  compileProject 'joypad'
-  compileProject "shrunk"
-  compileProject "shrunk_centroid"
-  # compileProject "shrunk-physic" 1 --todo
-  compileProject "sprite"
-  compileProject "test_animated_sprite"
-  compileProject "test_animated_sprite_physic"
-  compileProject "test_image"
-  compileProject "test_image_physic"
-  compileProject "test_same_palette"
-  compileProject "test_scroller"
-}
-
-function runProjects {
-  runProject "bullet"
-  runProject "collide"
-  runProject "collide_complex"
-  runProject "collide_multiple"
-  runProject "fixed_value"
-  runProject "hello"
-  runProject 'joypad'
-  runProject "shrunk"
-  runProject "shrunk_centroid"
-  # runProject "shrunk_physic" --todo
-  runProject "sprite"
-  runProject "test_animated_sprite"
-  runProject "test_animated_sprite_physic"
-  runProject "test_image"
-  runProject "test_image_physic"
-  runProject "test_same_palette"
-  runProject "test_scroller"
-}
-
 function _main {
   write-host $op $makeArg $killTime
-  if ($op -eq "compile") { compileProjects }
-  if ($op -eq "run") { runProjects }
+
+  compileProject "bullet"
+  runProject "bullet"
+
+  compileProject "collide"
+  runProject "collide"
+
+  compileProject "collide_complex"
+  runProject "collide_complex"
+
+  compileProject "collide_multiple"
+  runProject "collide_multiple"
+
+  compileProject "fixed_value"
+  runProject "fixed_value"
+
+  compileProject "hello"
+  runProject "hello"
+
+  compileProject 'joypad'
+  runProject 'joypad'
+
+  compileProject "shrunk"
+  runProject "shrunk"
+
+  compileProject "shrunk_centroid"
+  runProject "shrunk_centroid"
+
+  # runProject "shrunk_physic" --todo
+
+  compileProject "sprite"
+  runProject "sprite"
+
+  compileProject "test_animated_sprite"
+  runProject "test_animated_sprite"
+
+  compileProject "test_animated_sprite_physic"
+  runProject "test_animated_sprite_physic"
+
+  compileProject "test_image"
+  runProject "test_image"
+
+  compileProject "test_image_physic"
+  runProject "test_image_physic"
+
+  compileProject "test_same_palette"
+  runProject "test_same_palette"
+
+  compileProject "test_scroller"
+  runProject "test_scroller"
 }
 
 _main $op $makeArg $killTime
