c7c4d20a5b7e4423312bdf363192e37b68533fc4
David Vandensteen <dvandensteen@gmail.com>
2022-10-01 19:44:21 +0200

    Update : projects

--- scripts/compile-run-tester.ps1
+++ scripts/compile-run-tester.ps1
@@ -7,6 +7,7 @@ param (
 function compileProject($name) {
   pushd ..\projects\$name
   .\mak.bat clean
+  if ($name -eq "CDDA") { .\download-assets.bat }
   if ($LASTEXITCODE -ne 0) { Write-Host "break"; Write-Host $env:error; break }
   .\mak.bat run
   if ($LASTEXITCODE -ne 0) { break }
@@ -17,23 +18,27 @@ function compileProject($name) {
 function _main {
   write-host $op $makeArg $killTime
   compileProject "bullet"
+  compileProject "CDDA"
   compileProject "collide"
   compileProject "collide_complex"
   compileProject "collide_multiple"
   compileProject "DATdemo"
   compileProject "fixed_value"
   compileProject "hello"
-  compileProject 'joypad'
+  compileProject "joypad"
+  compileProject "palette_swap"
   compileProject "shrunk"
   compileProject "shrunk_centroid"
 
-  # runProject "shrunk_physic" --todo
+  # TODO : fix project
+  # compileProject "shrunk_physic"
 
   compileProject "sprite"
   compileProject "test_animated_sprite"
   compileProject "test_animated_sprite_physic"
   compileProject "test_image"
   compileProject "test_image_physic"
+  compileProject "test_limit"  
   compileProject "test_palettemanager"
   compileProject "test_same_palette"
   compileProject "test_scroller"
