56f33924c3e7aaab89fbc4d83d0986db88e7c25f
David Vandensteen <dvandensteen@gmail.com>
2022-10-01 21:02:04 +0200

    Remove : test_cart project

--- projects/test_cart/mak.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set projectName="test_cart"
-set builderScript="..\..\scripts\Builder-Manager.ps1"
-
-if "%1"=="" (
-  powershell -ExecutionPolicy Bypass -File %builderScript%  -ProjectName %projectName%
-) else (
-  powershell -ExecutionPolicy Bypass -File %builderScript% -ProjectName %projectName% -Rule %1
-)
