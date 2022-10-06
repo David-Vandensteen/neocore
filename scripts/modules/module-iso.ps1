3201346b552a1afe74dfa5a1bfc2bd6ff67ca992
David Vandensteen <dvandensteen@gmail.com>
2022-10-06 10:20:14 +0200

    Change : just in time import module

--- scripts/modules/module-iso.ps1
+++ scripts/modules/module-iso.ps1
@@ -40,3 +40,45 @@ function Write-ISO {
     exit 1
   }
 }
+
+function Write-CUE {
+  param (
+    [Parameter(Mandatory=$true)][String] $OutputFile,
+    [Parameter(Mandatory=$true)][String] $ISOName,
+    [System.Xml.XmlElement] $Config
+  )
+  function Get-CUETrack {
+    param (
+      [Parameter(Mandatory=$true)][String] $File,
+      [Parameter(Mandatory=$true)][int] $Index,
+      [Parameter(Mandatory=$true)][String] $Pregap
+    )
+    return (
+      'FILE "{0}" WAVE
+  TRACK {1:d2} AUDIO
+    PREGAP {2}
+    INDEX 01 00:00:00') -f $File, $Index, $Pregap
+  }
+
+  ('FILE "{0}" BINARY ' -f $ISOName) | Out-File -Encoding utf8 -FilePath $OutputFile -Force
+  "  TRACK 01 MODE1/2048 " | Out-File -Encoding utf8 -FilePath $OutputFile -Append -Force
+  "    INDEX 01 00:00:00 " | Out-File -Encoding utf8 -FilePath $OutputFile -Append -Force
+
+  if ($Config) {
+    $tracks = $Config.tracks.track
+    $tracks | ForEach-Object {
+      Get-CUETrack -File $_.file -Index $_.id -Pregap $_.pregap | Out-File -Encoding utf8 -FilePath $OutputFile -Append -Force
+    }
+  }
+  (Get-Content -Path $OutputFile -Raw).Replace("`r`n","`n") | Set-Content -Path $OutputFile -Force -NoNewline
+
+  if ((Test-Path -Path $OutputFile) -eq $true) {
+    Write-Host "builded CUE is available to $OutputFile" -ForegroundColor Green
+    Write-Host ""
+  } else {
+    Write-Host "error - $OutputFile was not generated" -ForegroundColor Red
+    exit 1
+  }
+}
+
+
