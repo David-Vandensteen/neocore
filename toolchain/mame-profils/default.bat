389e6188ebac51f62100502d44bc24918f0924b2
David Vandensteen <dvandensteen@gmail.com>
2022-10-22 13:37:40 +0200

    Update : mame profil path

--- toolchain/mame-profils/default.bat
+++ toolchain/mame-profils/default.bat
@@ -1,3 +1,7 @@
 @echo off
-set OUTFILE=..\..\build\mame-args.xml
+if exist ..\..\build set OUTFILE=..\..\build\mame-args.xml
+if exist ..\build set OUTFILE=..\build\mame-args.xml
+if exist buid set OUTFILE=build\mame-args.xml
+echo %OUTFILE%
+
 del %OUTFILE% /F /Q
--- toolchain/mame-profils/frameskip.bat
+++ toolchain/mame-profils/frameskip.bat
@@ -1,5 +1,9 @@
 @echo off
-set OUTFILE=..\..\build\mame-args.xml
+if exist ..\..\build set OUTFILE=..\..\build\mame-args.xml
+if exist ..\build set OUTFILE=..\build\mame-args.xml
+if exist buid set OUTFILE=build\mame-args.xml
+echo %OUTFILE%
+
 echo ^<?xml version="1.0" encoding="UTF-8" ?^> > %OUTFILE%
 echo ^<mame^> >> %OUTFILE%
 echo   ^<args^>-window -sound none -frameskip 10^</args^> >> %OUTFILE%
