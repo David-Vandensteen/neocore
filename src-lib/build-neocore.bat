bf16ae3c17622d3b8e76e7b72057762abd577500
David Vandensteen <dvandensteen@gmail.com>
2022-10-22 19:33:45 +0200

    Update : build neocore search sdk path

--- src-lib/build-neocore.bat
+++ src-lib/build-neocore.bat
@@ -1,8 +1,13 @@
 @echo off
-rem TODO : parmetric folder bin & neodev
-rem set backupPath=%path%
-rem set NEODEV=..\build\neodev-sdk
-rem path=%NEODEV%\m68k\bin;build\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
+
+set backupPath=%path%
+
+if exist ..\..\build set NEODEV=..\..\build\neodev-sdk
+if exist ..\build set NEODEV=..\build\neodev-sdk
+if exist build set NEODEV=build\neodev-sdk
+
+path=%NEODEV%\m68k\bin;build\bin;%windir%\System32;%windir%\System32\WindowsPowerShell\v1.0\
+
 set error=0
 @echo on
 make -f Makefile %1 %2 %3
@@ -11,5 +16,5 @@ if %errorlevel% neq 0 (
   set error=%errorlevel%
 )
 @echo off
-rem path=%backupPath%
+path=%backupPath%
 exit /b %error%
