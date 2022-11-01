6b376dc449d8aa723f249d5551475023ec9c5f45
David Vandensteen <dvandensteen@gmail.com>
2022-11-01 15:30:13 +0100

    Update : test animated sprite sample

--- samples/test_animated_sprite/main.c
+++ samples/test_animated_sprite/main.c
@@ -4,21 +4,19 @@
 
 NEOCORE_INIT
 
-Animated_Sprite player;
+GFX_Animated_Sprite player;
 
 int main(void) {
   gpu_init();
-  animated_sprite_init(&player, &player_sprite, &player_sprite_Palettes);
+  gfx_animated_sprite_init(&player, &player_sprite, &player_sprite_Palettes);
   flash_init(&player.flash, true, 10, 10);
-  animated_sprite_display(&player, 100, 100, PLAYER_SPRITE_ANIM_IDLE);
+  gfx_animated_sprite_display(&player, 100, 100, PLAYER_SPRITE_ANIM_IDLE);
   shrunk(player.as.baseSprite, player.si->maxWidth, shrunk_forge(0x5, 0xFF));
   while(1) {
     wait_vbl();
     logger_init();
-    // animated_sprite_move(&player, 1, 0);
-    // animated_sprite_set_position(&player, 10, 10);
-    animated_sprite_animate(&player);
-    animated_sprite_flash(&player);
+    gfx_animated_sprite_animate(&player);
+    gfx_animated_sprite_flash(&player);
     logger_animated_sprite("PLAYER", &player);
     SCClose();
   };
