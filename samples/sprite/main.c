512b65cdf5425acee9931ef2f85d97f8bf7ef4ce
David Vandensteen <dvandensteen@gmail.com>
2022-11-06 00:35:01 +0100

    Update : sprite sample

--- samples/sprite/main.c
+++ samples/sprite/main.c
@@ -1,41 +1,38 @@
 #include <neocore.h>
 #include "externs.h"
 
-NEOCORE_INIT
-
 int main(void) {
   GFX_Animated_Sprite player;
   GFX_Scroller background;
-  GFX_Image planet;
-  gpu_init();
-  gfx_image_init(&planet, &planet04_sprite, &planet04_sprite_Palettes);
-  gfx_animated_sprite_init(&player, &player_sprite, &player_sprite_Palettes);
-  gfx_scroller_init(&background, &background_sprite, &background_sprite_Palettes);
+  GFX_Picture planet;
+  init_gpu();
+  init_gp(&planet, &planet04_sprite, &planet04_sprite_Palettes);
+  init_gas(&player, &player_sprite, &player_sprite_Palettes);
+  init_gs(&background, &background_sprite, &background_sprite_Palettes);
 
-  // scroller_display(&background, &background_sprite, &background_sprite_Palettes, 0, 0);
-  gfx_scroller_display(&background, 0, 0);
-  gfx_image_display(&planet, 20, 100);
-  gfx_animated_sprite_display(&player, 10, 10, PLAYER_SPRITE_ANIM_IDLE);
+  display_gs(&background, 0, 0);
+  display_gp(&planet, 20, 100);
+  display_gas(&player, 10, 10, PLAYER_SPRITE_ANIM_IDLE);
   while(1) {
     wait_vbl();
-    joypad_update();
-    if (joypad_is_left() && player.as.posX > 0) { gfx_animated_sprite_move(&player, -1, 0); }
-    if (joypad_is_right() && player.as.posX < 280) { gfx_animated_sprite_move(&player, 1, 0); }
+    update_joypad();
+    if (joypad_is_left() && player.as.posX > 0) { move_gas(&player, -1, 0); }
+    if (joypad_is_right() && player.as.posX < 280) { move_gas(&player, 1, 0); }
     if (joypad_is_up() && player.as.posY > 0) {
-      gfx_animated_sprite_move(&player, 0, -1);
-      gfx_animated_sprite_set_animation(&player, PLAYER_SPRITE_ANIM_UP);
+      move_gas(&player, 0, -1);
+      set_anim_gas(&player, PLAYER_SPRITE_ANIM_UP);
     }
     if (joypad_is_down() && player.as.posY < 200) {
-      gfx_animated_sprite_move(&player, 0, 1);
-      gfx_animated_sprite_set_animation(&player, PLAYER_SPRITE_ANIM_DOWN);
+      move_gas(&player, 0, 1);
+      set_anim_gas(&player, PLAYER_SPRITE_ANIM_DOWN);
     }
-    if (!joypad_is_down() && !joypad_is_up()) { gfx_animated_sprite_set_animation(&player, PLAYER_SPRITE_ANIM_IDLE); }
+    if (!joypad_is_down() && !joypad_is_up()) { set_anim_gas(&player, PLAYER_SPRITE_ANIM_IDLE); }
 
-    gfx_scroller_move(&background, 1, 0);
-    if (background.s.scrlPosX > 512) scrollerSetPos(&background.s, 0, background.s.scrlPosY);
-    gfx_animated_sprite_animate(&player);
-    SCClose();
+    move_gs(background, 1, 0);
+    if (background.s.scrlPosX > 512) set_pos_gs(&background.s, 0, background.s.scrlPosY);
+    update_anim_gas(&player);
+    close_vbl();
   };
-  SCClose();
+  close_vbl();
   return 0;
 }
