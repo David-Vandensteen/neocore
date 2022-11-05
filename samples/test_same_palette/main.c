40936730659ee2d781b89879cdaaf8b206ee5217
David Vandensteen <dvandensteen@gmail.com>
2022-11-06 00:50:59 +0100

    Update : test same palette sample

--- samples/test_same_palette/main.c
+++ samples/test_same_palette/main.c
@@ -2,25 +2,23 @@
 #include <math.h>
 #include "externs.h"
 
-NEOCORE_INIT
-
-static GFX_Image planet;
+static GFX_Picture planet;
 static GFX_Animated_Sprite player;
 static GFX_Scroller backgroung;
 
 int main(void) {
-  gpu_init();
-  gfx_scroller_init(&backgroung, &background_sprite, &background_sprite_Palettes);
-  gfx_image_init(&planet, &planet04_sprite, &background_sprite_Palettes);
-  gfx_animated_sprite_init(&player, &player_sprite, &background_sprite_Palettes);
+  init_gpu();
+  init_gs(&backgroung, &background_sprite, &background_sprite_Palettes);
+  init_gp(&planet, &planet04_sprite, &background_sprite_Palettes);
+  init_gas(&player, &player_sprite, &background_sprite_Palettes);
 
-  gfx_scroller_display(&backgroung, 0, 0);
-  gfx_image_display(&planet, 100, 150);
-  gfx_animated_sprite_display(&player, 150, 10, PLAYER_SPRITE_ANIM_IDLE);
+  display_gs(&backgroung, 0, 0);
+  display_gp(&planet, 100, 150);
+  display_gas(&player, 150, 10, PLAYER_SPRITE_ANIM_IDLE);
 
   while(1) {
     wait_vbl();
-    logger_init();
+    init_logger();
     logger_byte("PALI PLAYER ", player.as.basePalette);
     logger_byte("PALI PLANET ", planet.pic.basePalette);
     logger_byte("PALI BACK   ", backgroung.s.basePalette);
@@ -28,9 +26,9 @@ int main(void) {
     logger_word("PAL PLAYER ", (int)player.pali);
     logger_word("PAL PLANET ", (int)planet.pali);
     logger_word("PAL BACK   ", (int)backgroung.pali);
-    gfx_animated_sprite_animate(&player);
-    SCClose();
+    update_anim_gas(&player);
+    close_vbl();
   };
-  SCClose();
+  close_vbl();
   return 0;
 }
