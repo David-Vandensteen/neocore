f5ff49ba880f7f2262864504bb04e3f54171a3ef
David Vandensteen <dvandensteen@gmail.com>
2022-11-06 00:28:45 +0100

    Update : shrunk centroid sample

--- samples/shrunk_centroid/main.c
+++ samples/shrunk_centroid/main.c
@@ -6,7 +6,7 @@ NEOCORE_INIT
 #define LOGO1_POSITION_CENTER_X_INIT 160
 #define LOGO3_POSITION_CENTER_Y_INIT 100
 
-static GFX_Image logo1, logo2, logo3;
+static GFX_Picture logo1, logo2, logo3;
 
 static BYTE logo1_shrunk_x = 0xF;
 static short logo1_shrunk_inc = -1;
@@ -26,19 +26,19 @@ static void update_logo2();
 static void update_logo3();
 
 static void init() {
-  logger_init();
+  init_logger();
   vec2short_init(&logo1_position_center, LOGO1_POSITION_CENTER_X_INIT, 30);
   vec2short_init(&logo2_position_center, 160, 180);
   vec2short_init(&logo3_position_center, 160, LOGO3_POSITION_CENTER_Y_INIT);
-  gfx_image_init(&logo1, &logo_sprite, &logo_sprite_Palettes);
-  gfx_image_init(&logo2, &logo_sprite, &logo_sprite_Palettes);
-  gfx_image_init(&logo3, &logo_sprite, &logo_sprite_Palettes);
+  init_gp(&logo1, &logo_sprite, &logo_sprite_Palettes);
+  init_gp(&logo2, &logo_sprite, &logo_sprite_Palettes);
+  init_gp(&logo3, &logo_sprite, &logo_sprite_Palettes);
 }
 
 static void display() {
-  gfx_image_display(&logo1, logo1_position_center.x, logo1_position_center.y);
-  gfx_image_display(&logo2, logo2_position_center.x, logo2_position_center.y);
-  gfx_image_display(&logo3, logo3_position_center.x, logo3_position_center.y);
+  display_gp(&logo1, logo1_position_center.x, logo1_position_center.y);
+  display_gp(&logo2, logo2_position_center.x, logo2_position_center.y);
+  display_gp(&logo3, logo3_position_center.x, logo3_position_center.y);
   logger_info("HORIZONTAL SHRUNK");
   logger_set_position(1, 11);
   logger_info("VERTICAL SHRUNK");
@@ -54,7 +54,7 @@ static void update() {
 
 static void update_logo1() { // Horizontal
   logo1_shrunk_x += logo1_shrunk_inc;
-  gfx_image_shrunk_centroid(&logo1, (short)logo1_position_center.x, (short)logo1_position_center.y, shrunk_forge(logo1_shrunk_x, 0xFF));
+  gfx_picture_shrunk_centroid(&logo1, (short)logo1_position_center.x, (short)logo1_position_center.y, shrunk_forge(logo1_shrunk_x, 0xFF));
   if (logo1_shrunk_x == 0) {
     logo1_shrunk_inc = 1;
     logo1_position_center.x = LOGO1_POSITION_CENTER_X_INIT;
@@ -66,14 +66,14 @@ static void update_logo1() { // Horizontal
 }
 
 static void update_logo2() { // Proportional
-  gfx_image_shrunk_centroid(&logo2, (short)logo2_position_center.x, (short)logo2_position_center.y, get_shrunk_proportional_table(shrunkTableIndex));
+  gfx_picture_shrunk_centroid(&logo2, (short)logo2_position_center.x, (short)logo2_position_center.y, get_shrunk_proportional_table(shrunkTableIndex));
   shrunkTableIndex++;
   if (shrunkTableIndex >= SHRUNK_TABLE_PROP_SIZE) shrunkTableIndex = 0;
 }
 
 static void update_logo3() { // Vertical
   logo3_shrunk_y += logo3_shrunk_inc;
-  gfx_image_shrunk_centroid(&logo3, (short)logo3_position_center.x, (short)logo3_position_center.y, shrunk_forge(0XF ,logo3_shrunk_y));
+  gfx_picture_shrunk_centroid(&logo3, (short)logo3_position_center.x, (short)logo3_position_center.y, shrunk_forge(0XF ,logo3_shrunk_y));
   if (logo3_shrunk_y == 0) {
     logo3_shrunk_inc = 1;
     logo3_position_center.y = LOGO3_POSITION_CENTER_Y_INIT;
@@ -85,14 +85,14 @@ static void update_logo3() { // Vertical
 }
 
 int main(void) {
-  gpu_init();
+  init_gpu();
   init();
   display();
   while(1) {
     wait_vbl();
     update();
-    SCClose();
+    close_vbl();
   };
-  SCClose();
+  close_vbl();
   return 0;
 }
