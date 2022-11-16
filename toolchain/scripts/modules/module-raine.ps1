8a455c8a28a1496c727c1de2c2daab77ded6b1e3
David Vandensteen <dvandensteen@gmail.com>
2022-11-16 16:04:01 +0100

    Update : configure bios path for Raine with Absolute path

--- toolchain/scripts/modules/module-raine.ps1
+++ toolchain/scripts/modules/module-raine.ps1
@@ -5,7 +5,8 @@ function Update-Raine {
     [Parameter(Mandatory=$true)][String] $Path
   )
   Write-Host "Configure Raine"
-  $content = [System.IO.File]::ReadAllText("$Path\config\raine32_sdl.cfg").Replace("/*neocd_bios*/","neocd_bios = $Path\roms\NEOCD.BIN")
+  $pathAbs = (Resolve-Path -Path $Path).Path
+  $content = [System.IO.File]::ReadAllText("$Path\config\raine32_sdl.cfg").Replace("/*neocd_bios*/","neocd_bios = $pathAbs\roms\NEOCD.BIN")
   [System.IO.File]::WriteAllText("$Path\config\raine32_sdl.cfg", $content)
 }
 
