4b2aa51f7b6c3231a3f63267c7c3837d80480262
David-Vandensteen <dvandensteen@gmail.com>
2024-06-02 15:23:06 +0200

    move toolchain watcher code

--- toolchain/scripts/Builder-Manager.ps1
+++ toolchain/scripts/Builder-Manager.ps1
@@ -94,7 +94,7 @@ function Main {
   if ($Rule -eq "clean") { exit 0 }
 
   Import-Module "$($Config.project.neocorePath)\toolchain\scripts\modules\module-raine.ps1"
-  Import-Module "$($Config.project.neocorePath)\toolchain\scripts\modules\module-watcher.ps1"
+  Import-Module "$($Config.project.neocorePath)\toolchain\scripts\modules\utils\watcher.ps1"
 
   Import-Module "$($Config.project.neocorePath)\toolchain\scripts\modules\services\writers\iso.ps1"
   Import-Module "$($Config.project.neocorePath)\toolchain\scripts\modules\services\writers\dist.ps1"
