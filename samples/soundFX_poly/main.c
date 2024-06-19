e2aff5f95106d54579ef53a6d907c799e50e3daa
David-Vandensteen <dvandensteen@gmail.com>
2024-06-19 13:33:40 +0200

    improve sound fx poly sample

--- samples/soundFX_poly/main.c
+++ samples/soundFX_poly/main.c
@@ -7,67 +7,54 @@
 #include "externs.h"
 #include "assets/sounds/fx/sfx.h"
 
-// TODO patch neocore
+// TODO patch neocore------------------------------------
 #define nc_wait_vbl() waitVBlank();
+#define stop_adpcm() send_sound_command(ADPCM_STOP);
+//---------------------------------------------------------
 
-
-static void sound1() {
-  send_sound_command(ADPCM_ALARM01);
-  nc_push_remaining_frame_adpcm_player(nc_second_to_frame(9));
-}
-
-static void sound2() {
-  send_sound_command(ADPCM_BELL);
-  nc_push_remaining_frame_adpcm_player(nc_second_to_frame(2));
-}
-
-static void sound3() {
-  send_sound_command(ADPCM_FAIL);
-  nc_push_remaining_frame_adpcm_player(nc_second_to_frame(2));
-}
+static void play_adpcm(adpcm) { send_sound_command(adpcm); }
 
 int main(void) {
-  nc_init_log();
   nc_set_joypad_edge_mode(true);
 
   while(1) {
     nc_update();
     nc_init_log();
-    nc_log("INTERACT WITH DIRECTION ON JOYPAD ...");
+    nc_set_position_log(0, 0);
+    nc_log("INTERACT WITH JOYPAD DIRECTION ...");
     nc_log("");
-    nc_log("LEFT: SOUNDFX 1");
-    nc_log("DOWN: SOUNDFX 2");
+    nc_log("");
+    nc_log("LEFT:  SOUNDFX 1");
+    nc_log("DOWN:  SOUNDFX 2");
     nc_log("RIGHT: SOUNDFX 3");
-    nc_log("UP: ALL SOUNDFX TOGETHER");
+    nc_log("UP:    ALL SOUNDFX TOGETHER");
 
     nc_set_position_log(3, 2);
     nc_debug_joypad(0);
 
     if (nc_joypad_is_up(0)) {
-      send_sound_command(ADPCM_STOP);
-      sound1();
+      stop_adpcm();
+      play_adpcm(ADPCM_ALARM01);
       nc_wait_vbl();
-      sound2();
+      play_adpcm(ADPCM_BELL);
       nc_wait_vbl();
-      sound3();
+      play_adpcm(ADPCM_FAIL);
     }
 
     if (nc_joypad_is_down(0)) {
-      send_sound_command(ADPCM_STOP);
-      sound2();
+      stop_adpcm();
+      play_adpcm(ADPCM_BELL);
     }
 
     if (nc_joypad_is_left(0)) {
-      send_sound_command(ADPCM_STOP);
-      sound1();
+      stop_adpcm();
+      play_adpcm(ADPCM_ALARM01);
     }
 
     if (nc_joypad_is_right(0)) {
-      send_sound_command(ADPCM_STOP);
-      sound3();
+      stop_adpcm();
+      play_adpcm(ADPCM_FAIL);
     }
-
-    nc_set_position_log(0, 0);
   };
 
   return 0;
