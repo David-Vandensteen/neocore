954a01e3efc645386a52f57996030b9585f645e4
David-Vandensteen <dvandensteen@gmail.com>
2025-03-24 21:35:52 +0100

    use absolute path in install component script

--- toolchain/scripts/modules/install/component.ps1
+++ toolchain/scripts/modules/install/component.ps1
@@ -6,17 +6,28 @@ function Install-Component {
     [Parameter(Mandatory=$true)][String] $PathDownload,
     [Parameter(Mandatory=$true)][String] $PathInstall
   )
+
+  if (-not($(Test-Path -Path $PathInstall))) {
+    Write-Host "  create $PathInstall" -ForegroundColor Yellow
+    New-Item -ItemType Directory -Path $PathInstall
+  }
+
   $fileName = Split-Path -Path $URL -Leaf
-  Logger-Info -Message "GET $fileName"
-  Start-Download -URL $URL -Path $PathDownload
-  if (-not($(Test-Path -Path "$PathDownload\$fileName"))) {
+  $pathDownload = Resolve-Path -Path $PathDownload
+  $pathInstall = Resolve-Path -Path $PathInstall
+
+  Write-Host "GET $fileName" -ForegroundColor Blue
+  Start-Download -URL $URL -Path $pathDownload
+
+  if (-not($(Test-Path -Path "$pathDownload\$fileName"))) {
     Write-Host "  error : download failed" -ForegroundColor Red
     exit 1
   }
-  Expand-Archive -Path "$PathDownload\$fileName" -DestinationPath $PathInstall -Force -ErrorAction Stop
 
-  Write-Host "  expanded $filename in $PathInstall" -ForegroundColor Yellow
+  Expand-Archive -Path "$pathDownload\$fileName" -DestinationPath $pathInstall -Force -ErrorAction Stop
+
+  Write-Host "  expanded $filename $pathInstall" -ForegroundColor Yellow
   Write-Host ""
 
-  Remove-Item -Path "$PathDownload\$fileName" -Force
+  Remove-Item -Path "$pathDownload\$fileName" -Force
 }
