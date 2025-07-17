23ba4e9c2b1070cd11379fe90c5830f47bbd31c2
David-Vandensteen <dvandensteen@gmail.com>
2025-07-17 18:13:54 +0200

    add specific path env for sprite rule

--- toolchain/scripts/modules/set/env-path.ps1
+++ toolchain/scripts/modules/set/env-path.ps1
@@ -6,7 +6,11 @@ function Set-EnvPath {
 
   $gccPath = Get-TemplatePath -Path $GCCPath
   $binPath = Get-TemplatePath -Path $Bin
-  $env:path = "$gccPath;$binPath;$env:windir\System32;$env:windir\System32\WindowsPowerShell\v1.0\"
+  if ($Rule -eq "sprite") {
+    $env:path = "$binPath"
+  } else {
+    $env:path = "$gccPath;$binPath;$env:windir\System32;$env:windir\System32\WindowsPowerShell\v1.0\"
+  }
 
   Write-Host "Env Path: $env:path"
   Write-Host "--------------------------------------------"
