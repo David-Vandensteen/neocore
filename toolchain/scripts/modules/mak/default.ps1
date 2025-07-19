614869b5beffab384f596a42c4750092bc13db28
David-Vandensteen <dvandensteen@gmail.com>
2025-07-19 16:20:47 +0200

    check char.bin file

--- toolchain/scripts/modules/mak/default.ps1
+++ toolchain/scripts/modules/mak/default.ps1
@@ -1,4 +1,9 @@
 function MakDefault {
   Write-Host "Mak default" -ForegroundColor Cyan
+  if (-not(Test-Path -Path "out\char.bin")) {
+    Write-Host "No out\char.bin found" -ForegroundColor Red
+    Write-Host "Run 'mak sprite' to generate it" -ForegroundColor Yellow
+    exit 1
+  }
   Build-Program
 }
