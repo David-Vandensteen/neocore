a8b410deebaeb9156917acb388601e899222e9ba
David-Vandensteen <dvandensteen@gmail.com>
2025-07-19 16:10:22 +0200

    clean remove out content folder

--- toolchain/scripts/modules/mak/clean.ps1
+++ toolchain/scripts/modules/mak/clean.ps1
@@ -1,6 +1,6 @@
-# TODO : remove out
 function MakClean {
-  $buildPathProject = "$($Config.project.buildPath)\$($Config.project.name)"
+  $projectBuildPath = Resolve-TemplatePath -Path $Config.project.buildPath
+  $buildPathProject = "$projectBuildPath\$($Config.project.name)"
   Write-Host "cleaning $buildPathProject" -ForegroundColor Yellow
 
   if (Test-Path -Path $buildPathProject) {
@@ -9,6 +9,16 @@ function MakClean {
       Get-ChildItem -Path $buildPathProject -Recurse -ErrorAction Stop | Remove-Item -Force -Recurse -ErrorAction Stop
     } catch {
       Write-Host "Failed to remove items: $_" -ForegroundColor Red
+    }
+  }
+
+  $outPath = "out"
+  if (Test-Path -Path $outPath) {
+    Write-Host "Removing files in $outPath" -ForegroundColor Yellow
+    try {
+      Get-ChildItem -Path $outPath -Recurse -ErrorAction Stop | Remove-Item -Force -Recurse -ErrorAction Stop
+    } catch {
+      Write-Host "Failed to remove items from out directory: $_" -ForegroundColor Red
     }
   }
 }
