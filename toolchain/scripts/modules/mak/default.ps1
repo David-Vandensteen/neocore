a84163a77c5814a3ca8e353a7d861f43a9a4d266
David-Vandensteen <dvandensteen@gmail.com>
2025-07-19 16:57:30 +0200

    fix test file

--- toolchain/scripts/modules/mak/default.ps1
+++ toolchain/scripts/modules/mak/default.ps1
@@ -1,7 +1,10 @@
 function MakDefault {
   Write-Host "Mak default" -ForegroundColor Cyan
-  if (-not(Test-Path -Path "out\char.bin")) {
-    Write-Host "No out\char.bin found" -ForegroundColor Red
+  $projectBuildPath = Resolve-TemplatePath -Path $Config.project.buildPath
+  $projectName = $Config.project.name
+  $file = "$projectBuildPath\$projectName\$projectName.cd"
+  if (-not(Test-Path -Path $file)) {
+    Write-Host "No $file found" -ForegroundColor Red
     Write-Host "Run 'mak sprite' to generate it" -ForegroundColor Yellow
     exit 1
   }
