f57ea1d5c4a27a92f2f2d82cb7aa59a2863fff54
David-Vandensteen <dvandensteen@gmail.com>
2025-07-19 15:43:29 +0200

    add externs header

---
+++ samples/CDDA/externs.h
@@ -0,0 +1,8 @@
+//scrollers
+extern scrollerInfo spectrum02_sprite;
+extern paletteInfo spectrum02_sprite_Palettes;
+
+//pictures
+extern pictureInfo k7_sprite;
+extern paletteInfo k7_sprite_Palettes;
+
