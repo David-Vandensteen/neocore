5e9acb3390c4be6400451dd93c91e1258afc84d8
David-Vandensteen <dvandensteen@gmail.com>
2025-07-23 10:00:56 +0200

    upgrade bullet sample

--- samples/bullet/main.c
+++ samples/bullet/main.c
@@ -1,25 +1,37 @@
 #include <neocore.h>
+#include <DATlib.h>
 #include <math.h>
-#include "player.h"
-#include "asteroid.h"
+#include "externs.h"
+
+// #include "player.h"
+// #include "asteroid.h"
+
+static GFX_Animated_Sprite player_sprite;
+// static GFX_Animated_Sprite_Physic player_sprite;
+// static GFX_Picture_Physic asteroid;
 
 static void init();
 static void display();
 static void update();
 
 static void init() {
-  player_init();
-  asteroid_init();
+  nc_init_gfx_animated_sprite(&player_sprite, &player_img, &player_img_Palettes);
 }
 
 static void display() {
-  player_display(100, 100);
-  asteroid_display();
+  nc_init_display_gfx_animated_sprite(
+    &player_sprite,
+    &player_img,
+    &player_img_Palettes,
+    100,
+    100,
+    PLAYER_IMG_ANIM_IDLE
+  );
 }
 
 static void update() {
-  player_update();
-  asteroid_update();
+  // player_update();
+  // asteroid_update();
 }
 
 int main(void) {
@@ -28,7 +40,8 @@ int main(void) {
 
   while(1) {
     nc_update();
-    update();
+    nc_update_animation_gfx_animated_sprite(&player_sprite);
+    // update();
   };
 
   return 0;
--- samples/bullet/player.c
+++ samples/bullet/player.c
@@ -6,18 +6,18 @@
 #include <neocore.h>
 #include "externs.h"
 #include "player.h"
-#include "bullet_player.h"
+// #include "bullet_player.h"
 
 static GFX_Animated_Sprite_Physic player_sprite;
 
 void player_init() {
   nc_init_gfx_animated_sprite_physic(&player_sprite, &player_img, &player_img_Palettes, 48, 16, 0, 0);
-  bullet_player_init();
+  // bullet_player_init();
 }
 
 void player_display(short x, short y) {
   nc_display_gfx_animated_sprite_physic(&player_sprite, x, y, PLAYER_IMG_ANIM_IDLE);
-  bullet_player_display(x, y);
+  // bullet_player_display(x, y);
 }
 
 void player_update() {
@@ -35,5 +35,5 @@ void player_update() {
   if (nc_joypad_is_right(0) && position.x < get_player_max_x()) nc_move_gfx_animated_sprite_physic(&player_sprite, get_player_speed(), 0);
 
   if (!nc_joypad_is_down(0) && !nc_joypad_is_up(0)) nc_set_animation_gfx_animated_sprite_physic(&player_sprite, PLAYER_IMG_ANIM_IDLE);
-  bullet_player_update(nc_joypad_is_a(0), position.x, position.y);
+  // bullet_player_update(nc_joypad_is_a(0), position.x, position.y);
 }
