15a03c70f1399e27eb1a3cdbb5de9e94d7a21d67
David-Vandensteen <dvandensteen@gmail.com>
2025-07-26 11:45:51 +0200

    upgrade bullet sample

--- samples/bullet/player.c
+++ samples/bullet/player.c
@@ -21,7 +21,8 @@ void player_display(short x, short y) {
 }
 
 void player_update() {
-  Vec2short position = nc_get_position_gfx_animated_sprite_physic(player_sprite);
+  Position position;
+  nc_get_position_gfx_animated_sprite_physic(&player_sprite, &position);
   nc_update_animation_gfx_animated_sprite_physic(&player_sprite);
   if (nc_joypad_is_up(0) && position.y > get_player_min_y()) {
     nc_move_gfx_animated_sprite_physic(&player_sprite, 0, -get_player_speed());
