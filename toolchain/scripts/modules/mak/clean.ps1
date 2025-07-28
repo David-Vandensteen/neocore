3bc169e81e13678440c7ceed5f2c8296ae8f9625
David-Vandensteen <dvandensteen@gmail.com>
2025-07-28 20:36:39 +0200

    error spread

--- toolchain/scripts/modules/mak/clean.ps1
+++ toolchain/scripts/modules/mak/clean.ps1
@@ -3,12 +3,15 @@ function MakClean {
   $buildPathProject = "$projectBuildPath\$($Config.project.name)"
   Write-Host "cleaning $buildPathProject" -ForegroundColor Yellow
 
+  $success = $true
+
   if (Test-Path -Path $buildPathProject) {
     Write-Host "Removing files in $buildPathProject" -ForegroundColor Yellow
     try {
       Get-ChildItem -Path $buildPathProject -Recurse -ErrorAction Stop | Remove-Item -Force -Recurse -ErrorAction Stop
     } catch {
       Write-Host "Failed to remove items: $_" -ForegroundColor Red
+      $success = $false
     }
   }
 
@@ -19,6 +22,9 @@ function MakClean {
       Get-ChildItem -Path $outPath -Recurse -ErrorAction Stop | Remove-Item -Force -Recurse -ErrorAction Stop
     } catch {
       Write-Host "Failed to remove items from out directory: $_" -ForegroundColor Red
+      $success = $false
     }
   }
+
+  return $success
 }
