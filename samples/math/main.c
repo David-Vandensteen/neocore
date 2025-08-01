468b201d712f7132ed6faa49b70da6f2fe6c4a22
David-Vandensteen <dvandensteen@gmail.com>
2025-08-01 12:23:27 +0200

    upgrade math sample

--- samples/math/main.c
+++ samples/math/main.c
@@ -5,12 +5,12 @@ int main(void) {
   FIXED val2 = nc_fix(10.5);
 
   nc_init_log();
-  nc_log_info("10.5 ADD 10.5 : %d", nc_fix_to_int(nc_fix_add(val1, val2)));
-  nc_log_info("11 MULT 2 : %d", nc_bitwise_multiplication_2(11));
-  nc_log_info("11 MULT 4 : %d", nc_bitwise_multiplication_4(11));
-  nc_log_info("40 DIV 4 : %d", nc_bitwise_division_4(40));
-  nc_log_info("MIN 15, 20 : %d", nc_min(15, 20));
-  nc_log_info("MAX 15, 20 : %d", nc_max(15, 20));
+  nc_log_info_line("10.5 ADD 10.5 : %d", nc_fix_to_int(nc_fix_add(val1, val2)));
+  nc_log_info_line("11 MULT 2 : %d", nc_bitwise_multiplication_2(11));
+  nc_log_info_line("11 MULT 4 : %d", nc_bitwise_multiplication_4(11));
+  nc_log_info_line("40 DIV 4 : %d", nc_bitwise_division_4(40));
+  nc_log_info_line("MIN 15, 20 : %d", nc_min(15, 20));
+  nc_log_info_line("MAX 15, 20 : %d", nc_max(15, 20));
   nc_log_info("ABS -11: %d", nc_abs(-11));
 
   while(1) {
