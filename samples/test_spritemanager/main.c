73d92855d9b1de3fb69bd75dfd38feb444195e7b
David-Vandensteen <dvandensteen@gmail.com>
2025-08-01 10:44:07 +0200

    upgrade test sprite manager sample

--- samples/test_spritemanager/main.c
+++ samples/test_spritemanager/main.c
@@ -23,11 +23,11 @@ static void display() {
 
 static void update() {
   nc_init_log();
-  nc_log_info("P1 INDEX : %04d", planet01.pictureDAT.baseSprite);
-  nc_log_info("MUST BE : 1");
-  nc_log_info("P2 INDEX : %04d", planet02.pictureDAT.baseSprite);
-  nc_log_info("MUST BE : 9");
-  nc_log_info("P3 INDEX : %04d", planet03.pictureDAT.baseSprite);
+  nc_log_info_line("P1 INDEX : %04d", planet01.pictureDAT.baseSprite);
+  nc_log_info_line("MUST BE : 1");
+  nc_log_info_line("P2 INDEX : %04d", planet02.pictureDAT.baseSprite);
+  nc_log_info_line("MUST BE : 9");
+  nc_log_info_line("P3 INDEX : %04d", planet03.pictureDAT.baseSprite);
   nc_log_info("MUST BE : 17");
 
   if (nc_get_frame_counter() == 500) {
