b6922c0a653d3faf22bbf87113916f15528534c4
David-Vandensteen <dvandensteen@gmail.com>
2025-08-01 11:30:01 +0200

    upgrade collide complex sample

---
+++ samples/collide_complex/out/charInclude.h
@@ -0,0 +1,11 @@
+
+//pictures
+extern const pictureInfo peak_sprite;
+extern const paletteInfo peak_sprite_Palettes;
+extern const pictureInfo dot_sprite;
+extern const paletteInfo dot_sprite_Palettes;
+
+//animated sprites
+extern const spriteInfo player_sprite;
+extern const paletteInfo player_sprite_Palettes;
+#include "../assets/gfx/player_sprite/player_sprite.h"
