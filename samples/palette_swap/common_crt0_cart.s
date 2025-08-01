4021cdc1b0670868a76d283a86425bfe515c1b9f
David-Vandensteen <dvandensteen@gmail.com>
2025-08-01 14:41:02 +0200

    upgrade palette swap sample

--- samples/palette_swap/common_crt0_cart.s
+++ samples/palette_swap/common_crt0_cart.s
@@ -1,13 +1,13 @@
 	* ++====================================================================++
-	* || common_crt0_cd.s - C Run Time Startup Code for Neo Geo CD			||
+	* || common_crt0_cart.s - C Run Time Startup Code for Neo Geo Cartridge	||
 	* ++--------------------------------------------------------------------++
-	* || $Id: common_crt0_cd.s,v 1.5 2001/07/13 14:46:31 fma Exp $			||
+	* || $Id: common_crt0_cart.s,v 1.5 2001/07/13 14:46:31 fma Exp $		||
 	* ++--------------------------------------------------------------------++
 	* || This is the startup code needed by programs compiled with GCC		||
 	* ++--------------------------------------------------------------------||
-	* || BGM: The Cranberries - Saving Grace								||
+	* || BGM: Guitar Vader - S.P.Y.											||
 	* ++====================================================================++
-	
+
 _ZERO_DIVIDE	=	0x00c00426
 _CHK_CMD	=	0x00c00426
 _TRAPV_CMD	=	0x00c00426
@@ -17,7 +17,7 @@ _IRQ4	=	0x00c00426
 _IRQ5	=	0x00c00426
 _IRQ6	=	0x00c00426
 _IRQ7	=	0x00c00426
-	
+
 ********************** Exported Symbols **********************
 	.globl	_start
 	.globl	atexit
@@ -29,13 +29,13 @@ _IRQ7	=	0x00c00426
 	.globl	memset
 	.globl	__bss_start	
 	.globl	_end
-	.globl	_sbrk_base1
-	.globl	_sbrk_base2
 
 ********************** Program Start *************************
 
+** NOTE: Cartridge systems have swapped IRQ1 and IRQ2
+
 	.org	0x0000
-	
+
 	.long	0x0010f300	|;reset stack ptr
 	.long	0x00c00402	/*;reset ptr*/
 	.long	0x00c00408	/*;bus error*/
@@ -54,98 +54,80 @@ _IRQ7	=	0x00c00426
 	.long	0x00c00426, 0x00c00426, 0x00c00426	/*;unused*/
 	.long	0x00c0042c	/*;uninitialized interrupt*/
 
-	|;.long	0x00c00426,	0x00c00426,	0x00c00426,	0x00c00426	/*;unused*/
-	|;.long	0x00c00426,	0x00c00426,	0x00c00426,	0x00c00426	/*;unused*/
-	|;.long	0x00c00432	/*;virtual interrupt*/
-	.long	0xC00522, 0xC00528, 0xC0052E, 0xC00534
-	.long	0xC0053A, 0xC004F2, 0xC004EC, 0xC004E6
-	.long	0xC004E0
-	
+	.long	0x00c00426,	0x00c00426,	0x00c00426,	0x00c00426	/*;unused*/
+	.long	0x00c00426,	0x00c00426,	0x00c00426,	0x00c00426	/*;unused*/
+	.long	0x00c00432	/*;virtual interrupt*/
 	|; 0x64
-	.long	_IRQ1, _IRQ2, _IRQ3, _IRQ4, _IRQ5, _IRQ6, _IRQ7	/*;4-7 unused*/
-	.long	TRAP00,	TRAP01,	TRAP02,	TRAP03
-	.long	TRAP04,	TRAP05,	TRAP06,	TRAP07
-	.long	TRAP08,	TRAP09,	TRAP10,	TRAP11
-	.long	TRAP12,	TRAP13,	TRAP14,	TRAP15
-	.long	0xC00426, 0xC00426, 0xC00426, 0xC00426
-	.long	0xC00426, 0xC00426, 0xC00426, 0xC00426
-	.long	0xC00426, 0xC00426, 0xC00426, 0xC00426
-	.long	0xC00426, 0xC00426, 0xC00426, 0xC00426
-	
+	.long	_IRQ2, _IRQ1, _IRQ3, _IRQ4, _IRQ5, _IRQ6, _IRQ7	/*;4-7 unused*/
+
+__security_code:
+	.long	0x76004a6d, 0x0a146600, 0x003c206d, 0x0a043e2d
+	.long	0x0a0813c0, 0x00300001, 0x32100c01, 0x00ff671a
+	.long	0x30280002, 0xb02d0ace, 0x66103028, 0x0004b02d
+	.long	0x0acf6606, 0xb22d0ad0, 0x67085088, 0x51cfffd4
+	.long	0x36074e75, 0x206d0a04, 0x3e2d0a08, 0x3210e049
+	.long	0x0c0100ff, 0x671a3010, 0xb02d0ace, 0x66123028
+	.long	0x0002e048, 0xb02d0acf, 0x6606b22d, 0x0ad06708
+	.long	0x588851cf, 0xffd83607
+	.word	0x4e75
+
 	.org	0x100
-	.ascii	"NEO-GEO"
-	.byte	_CDDA_FLAG
+	.ascii	"NEO-GEO\0"
 	.word	_NGH
 	.long	_PROGRAM_SIZE
 	.long	_WRK_BCKP_AREA
 	.word	_WRK_BCKP_AREA_SIZE
+	
 	.byte	_EYE_CATCHER
 	.byte	_EYE_CATCHER_TILES
-
+	
 	.long	JPconfig
 	.long	NAconfig
 	.long	EUconfig
 	
-	jmp		_ENTRY_USER
-	jmp		_ENTRY_PLAYER_START
-	jmp		_ENTRY_DEMO_END
-	jmp		_ENTRY_COIN_SOUND
+	jmp	_ENTRY_USER
+	jmp	_ENTRY_PLAYER_START
+	jmp	_ENTRY_DEMO_END
+	jmp	_ENTRY_COIN_SOUND
 
-	.word	0xffff
-	.long	0xffffffff
-	.long	0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff
-	.long	0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff
-	.long	0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff
-	.long	0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff
-
-|;* Standard IRQ1 handler
-_irq1_handler:
-	move.w	#2, 0x3C000C
-	rte
-|;* Standard IRQ3 handler
 _irq3_handler:
 	move.w	#1, 0x3C000C
 _dummy_exc_handler:
 	rte
-|;* Dummy atexit (does nothing for now)
-atexit:
+atexit:	|;Dummy atexit (does nothing for now)
 	moveq	#0, d0
 _dummy_config_handler:
 	rts
 
+_irq1_handler:	|;Standard IRQ1 handler
+	move.w	#2, 0x3C000C
+	rte
+
+	.org	0x182
+	.long	__security_code
+
+_dummyTIdata:
+	.word	0x0000
+
 |;* Entry point of our program
 _start:
 	|;* Setup stack pointer and 'system' pointer
-	|;lea		0x10F300, a7
-	|;lea		0x108000, a5
+	|;lea		0x10F300,a7
+	|;lea		0x108000,a5
 
 	|;* Reset watchdog
 	move.b	d0, 0x300001
-	
+
 	moveq	#0, d0
 	move.l	d0, TInextTable
 	move.l	d0, VBL_callBack
 	move.l	d0, VBL_skipCallBack
-	
-	|;* Enable manual reset (A+B+C+START or SELECT)
-	bclr	#7, 0x10FD80
 
 	|;* Flush interrupts
 	move.b	#7, 0x3C000C
-
-	|;* Enable interrupts
-	move.w	#0x2000, sr
 	
-	|;* Stop CDDA
-	move.w	#0x0200, d0
-	jsr		0xc0056a
-
-	|;* Initialize base pointers for malloc
-	move.l	#0x120000, _sbrk_base2
-	move.l	#_end, d0
-	add.l	#32, d0
-	and.l	#0xFFFFFF0, d0
-	move.l	d0, _sbrk_base1
+	|;* Enable interrupts
+	move.w	#0x2000,sr
 	
 	|;* Initialize BSS section
 	move.l	#_end, d0
@@ -153,10 +135,10 @@ _start:
 	move.l	d0, -(a7)
 	clr.l	-(a7)
 	pea		__bss_start
-	jbsr	memset	
+	jbsr	memset
 	
-	|;hotfix
-	move.b #0x80, 0x10fd80
+	jsr	0xc004c2	|; FIX_CLEAR
+	jsr	0xc004c8	|; LSP_1st
 	
 	|;* Jump to main
 	jbsr	main
@@ -164,5 +146,7 @@ _start:
 	|;* Call global destructors
 	jbsr	__do_global_dtors
 
-	|;* For CD systems, return to CD Player
-	jmp		0xC0055E
+	|;* For cart systems, infinite loop
+9:
+	jmp	9b(pc)
+
