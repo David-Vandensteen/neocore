082af0983eaa06af86726d7a66f1bed356b1a159
David-Vandensteen <dvandensteen@gmail.com>
2025-08-02 13:40:15 +0200

    Removes player sprite assets
    Removes the player sprite and related animation data.
    This change is likely part of a larger refactoring
    or removal of a feature that depends on these assets.

--- samples/test_limit/out/charInclude.h
+++ samples/test_limit/out/charInclude.h
@@ -5,8 +5,3 @@ extern const paletteInfo background_sprite_Palettes;
 //pictures
 extern const pictureInfo planet04_sprite;
 extern const paletteInfo planet04_sprite_Palettes;
-
-//animated sprites
-extern const spriteInfo player_sprite;
-extern const paletteInfo player_sprite_Palettes;
-#include "../assets/gfx/player_sprite/player_sprite.h"
