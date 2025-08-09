7861c471d1505b737cc770d4d4bb93f2cdbd0f26
David-Vandensteen <dvandensteen@gmail.com>
2025-08-09 15:17:31 +0200

    Removes test_v2tov3 sample
    This commit removes the test_v2tov3 sample, which was used
    for testing the migration from Neocore v2 to v3. The sample
    is no longer needed as the migration process is complete.

--- samples/test_v2tov3/mak.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set projectSetting=project.xml
-
-for /d %%i in (..\..) do set neocorePath=%%~fi
-
-cd /d %neocorePath%
-powershell -ExecutionPolicy Bypass -File bootstrap\scripts\project\mak.ps1 -ProjectSetting %projectSetting% %*
--- samples/test_v2tov3/mak.ps1
+++
@@ -1,5 +0,0 @@
-$projectSetting = "project.xml"
-$neocorePath = Resolve-Path -Path "..\.."
-
-cd $neocorePath
-powershell -ExecutionPolicy Bypass -File bootstrap\scripts\project\mak.ps1 -ProjectSetting $projectSetting $args
